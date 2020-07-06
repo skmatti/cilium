7ee52d6a6efbdd9a0a3aec3abc5dde87a94dc34f
Yongkun Gui <ygui@google.com>
2020-07-06 11:37:23 -0700

    Add google-diagnose.sh in cilium image
    The image is used for gkectl diagnose to dump cilium debug info
    
    BUG: 148531082
    Change-Id: If934b053e4c7e49db2bd246fec3bdf7e3adbc8b5

---
+++ google-diagnose.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script to collect cilium health stats for gke snapshot
+
+cilium version
+cilium status --verbose
+cilium-health status
+cilium debuginfo
+
+cilium config
+cilium node list
+cilium metrics list
+cilium identity list
+cilium policy selectors -o json
+
+bpftool map show
+bpftool prog show
+bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb4_services_v2
+bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb4_services
+bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb4_backends
+bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_services_v2
+bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_services
+bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_backends
+
+cilium map list --verbose
+cilium bpf tunnel list
+cilium bpf lb list
+cilium bpf endpoint list
+cilium bpf ipcache list
+cilium bpf sha list
