5323cadff8e1f3e11cfd19fe9b96950f24cef5c2
Weilong Cui <cuiwl@google.com>
2020-09-11 15:16:00 -0700

    Parametrizes build_and_push_to_gcr.sh
    Change-Id: I3ecc3b5826c01dc30b8cbea8b5aaf283d4a0511a

--- google_test/build_and_push_to_gcr.sh
+++ google_test/build_and_push_to_gcr.sh
@@ -3,8 +3,10 @@
 # This can be invoked by hand to build and push local dev cilium and operator
 # images to dev project registry for further testing.
 
-DOCKER_IMAGE_TAG=my-cilium make docker-image
-docker tag cilium/cilium:my-cilium gcr.io/$USER-gke-dev/cilium:my-cilium
-docker push gcr.io/$USER-gke-dev/cilium:my-cilium
-docker tag cilium/operator:my-cilium gcr.io/$USER-gke-dev/cilium-operator:my-cilium
-docker push gcr.io/$USER-gke-dev/cilium-operator:my-cilium
+TAG=my-cilium
+
+DOCKER_IMAGE_TAG=$TAG make docker-image
+docker tag cilium/cilium:$TAG gcr.io/$USER-gke-dev/cilium:$TAG
+docker push gcr.io/$USER-gke-dev/cilium:$TAG
+docker tag cilium/operator-generic:$TAG gcr.io/$USER-gke-dev/cilium-operator:$TAG
+docker push gcr.io/$USER-gke-dev/cilium-operator:$TAG
