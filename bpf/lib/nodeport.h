4bfc9daf553d092424d5fbc32a085415f4e36435
Akhil Velagapudi <avelagap@google.com>
2022-05-25 23:45:34 +0000

    Fix multi-network NodePort for local backends
    For MACVLAN interfaces, when a frame enters the lower device, if the
    destination MAC is not one of the sub-interfaces, the frame is processed
    by the host. This breaks isolation, and leads to a strange datapath
    where the kernel routes the packet back to the lower device where it's
    captured and redirected back to the ingress-side (by
    `multinic_redirect_ipv4`) for another attempt at reaching the pod.
    
    To avoid this, set the correct destination MAC after xlating the
    destination addr for multi-NIC endpoints.
    
    Also, redirecting nodeport egress packets to host interface by the
    `from-container` program doesn't work for MACVLAN because the program is
    running in the pod namespace. As a workaround, don't redirect and rely
    on the MACVLAN driver to properly egress the packet.
    
    Fix: b/233754304
    Change-Id: Id6165b129ad5fa311be8c0774b587f9347c50663
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/542981
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Varun Marupadi <varunmar@google.com>
    Reviewed-by: Maciej Skrocki <maciejskrocki@google.com>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -725,7 +725,7 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 	struct lb6_service *svc;
 	struct lb6_key key = {};
 	struct ct_state ct_state_new = {};
-	bool backend_local;
+	const struct endpoint_info *backend_local;
 	__u32 monitor = 0;
 
 	cilium_capture_in(ctx);
@@ -844,6 +844,13 @@ redo:
 			if (ret < 0)
 				return ret;
 		}
+	}
+
+	/* See comment NODEPORT_LOCAL_REDIRECT_MAC. */
+	if (backend_local && backend_local->flags & ENDPOINT_F_MULTI_NIC) {
+		mac_t dmac = backend_local->mac;
+		if (eth_store_daddr(ctx, (__u8 *) &dmac, 0) < 0)
+			return DROP_WRITE_ERROR;
 	}
 
 	if (!backend_local) {
@@ -1814,7 +1821,7 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 	struct lb4_service *svc;
 	struct lb4_key key = {};
 	struct ct_state ct_state_new = {};
-	bool backend_local;
+	const struct endpoint_info *backend_local;
 	__u32 monitor = 0;
 
 	cilium_capture_in(ctx);
@@ -1960,6 +1967,26 @@ redo:
 			if (ret < 0)
 				return ret;
 		}
+	}
+
+	/* NODEPORT_LOCAL_REDIRECT_MAC
+	 *
+	 * For MACVLAN interfaces, when a frame enters the lower device, if the
+	 * destination MAC is not one of the sub-interfaces, the frame is processed
+	 * by the host [1]. This breaks isolation, and leads to a strange datapath
+	 * where the kernel routes the packet back to the lower device where it's
+	 * captured and redirected back to the ingress-side (by
+	 * `multinic_redirect_ipv4`) for another attempt at reaching the pod.
+	 *
+	 * To avoid this, we can set the correct destination MAC after xlating the
+	 * destination addr.
+	 *
+	 * [1]: https://vincent.bernat.ch/en/blog/2017-linux-bridge-isolation#about-macvlan-interfaces
+	 */
+	if (backend_local && backend_local->flags & ENDPOINT_F_MULTI_NIC) {
+		mac_t dmac = backend_local->mac;
+		if (eth_store_daddr(ctx, (__u8 *) &dmac, 0) < 0)
+			return DROP_WRITE_ERROR;
 	}
 
 	if (!backend_local) {
@@ -2202,6 +2229,10 @@ int tail_rev_nodeport_lb4(struct __ctx_buff *ctx)
 	edt_set_aggregate(ctx, 0);
 	cilium_capture_out(ctx);
 
+/* Redirect is not supported for ipvlan/macvlan host interfaces: b/235141939 */
+#ifdef IS_MULTI_NIC_DEVICE
+	return CTX_ACT_OK;
+#endif
 	return ctx_redirect(ctx, ifindex, 0);
 }
 
