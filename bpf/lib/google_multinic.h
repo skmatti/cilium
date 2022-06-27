22cc1eef1d0f296c80f24e0abbc1aae061e5b7e3
Yifeng Shen <yfshen@google.com>
2022-06-27 16:44:52 -0700

    Skip policy enforcement for DHCP packets from multinic endpoint
    * DHCP reponses hairpin back on the same multinic interface should skip
      policy enforcement on the INGRESS program (to-container)
    * Create new tc_index flag TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP to
      construct the program to skip policy enforcement for google dhcp
      packets.
    * The reason to use tc_index is ebpf is already using the value to
      construct program to skip some operations. e.g.
      TC_INDEX_F_SKIP_INGRESS_PROXY. And this has less risk to be changed
      by kernel or other programs. Unlike ctx-cb which is usally cleared by
      bpf_clear_meta() at the beginning of program sections.
    
    BUG: b/236838910
    Change-Id: Ib34c5d448cce3db49cf81cd47f0332306da8ca84
    (cherry picked from commit a8b023f713ff18c80b0a5675792c2c97bb83ce96)
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/783883
    Reviewed-by: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Akhil Velagapudi <avelagap@google.com>
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>

--- bpf/lib/google_multinic.h
+++ bpf/lib/google_multinic.h
@@ -110,20 +110,41 @@ static __always_inline __maybe_unused int redirect_if_dhcp(struct __ctx_buff *ct
 	return CTX_ACT_OK;
 }
 
+static __always_inline __maybe_unused void skip_policy_if_dhcp(struct __ctx_buff *ctx __maybe_unused,
+                        __u8 nexthdr __maybe_unused, int l4_off __maybe_unused)
+{
+	return;
+}
+
 #else
+
+static __always_inline void
+ctx_skip_google_dhcp_set(struct __sk_buff *ctx)
+{
+	ctx->tc_index |= TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP;
+}
+
+static __always_inline bool ctx_skip_google_dhcp(struct __sk_buff *ctx)
+{
+	volatile __u32 tc_index = ctx->tc_index;
+
+	ctx->tc_index &= ~TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP;
+	return tc_index & TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP;
+}
 
 /**
  * Redirect dhcp client packets
  * if destination port is 67 on UDP(dhcp-request), redirect to pod-network interface
- * to be further sent to container for dhcp-server processing. if destination port is
- * 68 on UDP(dhcp-response), hairpin the packet from egress to ingress direction
- * on the same inteface on which the packet is seen by this program.
+ * to be further sent to container for dhcp-server processing.
+ * if destination port is 68 on UDP(dhcp-response), set TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP then
+ * hairpin the packet from egress to ingress direction on the same inteface on which
+ * the packet is seen by this program.
  * @arg ctx:      packet
  * @arg nexthdr:  l3 next header field
  * @arg l4_off:   offset to L4 header
  *
  * Return CTX_ACT_OK if the packet needs further processing.
- *        Or a possitive code returned by bpf_redirect where no futher processing needed.
+ *        Or a positive code returned by bpf_redirect where no further processing needed.
  *        A negative DROP_* code on error.
  */
 static __always_inline __maybe_unused int redirect_if_dhcp(struct __ctx_buff *ctx,
@@ -144,13 +165,45 @@ static __always_inline __maybe_unused int redirect_if_dhcp(struct __ctx_buff *ct
             send_trace_notify(ctx, TRACE_TO_STACK, 0, 0, 0,
                               ctx->ifindex,
                               REASON_GOOGLE_DHCP_RESP_REDIRECT, TRACE_PAYLOAD_LEN);
-
+            // DHCP response packets hairpin back on the same interface are self-generated
+            // from the endpoint and should not have policy enforcement after redirect.
+            // Set TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP before haripin.
+            // The tc_index value is extracted on the INGRESS of the same interface and
+            // constructs the program to skip policy enforcement.
+            ctx_skip_google_dhcp_set(ctx);
             return redirect(ctx->ifindex, BPF_F_INGRESS);
         }
 
     }
     return CTX_ACT_OK;
 }
+
+/**
+ * Skip policy enforcement for DHCP packets hairpin back from redirect_if_dhcp()
+ * @arg ctx:      packet
+ * @arg nexthdr:  l3 next header field
+ * @arg l4_off:   offset to L4 header
+ *
+ * Directly return if TC_INDEX_F_SKIP_POLICY_GOOGLE_DHCP is not set.
+ * Otherwise set CB_POLICY to skip policy enforcement for DHCP response packets.
+ */
+static __always_inline __maybe_unused void skip_policy_if_dhcp(struct __ctx_buff *ctx,
+                       __u8 nexthdr, int l4_off)
+{
+    __be16 dport __maybe_unused;
+    if (unlikely(ctx_skip_google_dhcp(ctx))) {
+        if (nexthdr == IPPROTO_UDP) {
+            if (l4_load_port(ctx, l4_off + UDP_DPORT_OFF, &dport) < 0)
+                return;
+            if (dport == bpf_htons(DHCP_RESPONSE_UDP_DPORT)) {
+                ctx_store_meta(ctx, CB_POLICY, 1);
+                return;
+            }
+        }
+    }
+    return;
+}
+
 #endif
 
 #endif /* __LIB_GOOGLE_MULTINIC_H_ */
