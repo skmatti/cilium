9b64c038b5509192ce8968f21bf5b5eb468d1f4f
Yifeng Shen <yfshen@google.com>
2022-06-28 17:37:41 -0700

    Skip multinic endpoint based on multi-network label in egress gateway
    TESTED: https://paste.googleplex.com/5239577770983424
    
    BUG: 237015200
    Change-Id: I8804825c7f4769badc8d99e848417ea5a7d01959
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/784994
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Akhil Velagapudi <avelagap@google.com>
    Reviewed-by: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>

---
+++ pkg/egressgateway/google_policy.go
@@ -0,0 +1,13 @@
+package egressgateway
+
+import (
+	k8sLbls "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels"
+	networkv1 "gke-internal.googlesource.com/anthos-networking/apis/v2/network/v1"
+)
+
+// isMultiNICEndpoint returns true if the endpoint has multi-network label
+// and not in the pod-network.
+func isMultiNICEndpoint(lblsToMatch k8sLbls.Labels) bool {
+	network := lblsToMatch.Get(networkv1.NetworkAnnotationKey)
+	return network != "" && network != networkv1.DefaultNetworkName
+}
