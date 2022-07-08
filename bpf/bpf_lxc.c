a9d1765e12b7a07c4f9a76893e493784b9fccd94
Jonathan Hart <jonohart@google.com>
2022-07-08 21:51:38 +0000

    Skip fast redirect for multinic interfaces.
    BUG: 238334553
    Change-Id: Iacc3034fb34bbeed838c285d28edfd5dc0fd4a73
    (cherry picked from commit 73e12fb79b0b3da534cb5221aa196eabb0e4e36f)
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/784339
    Reviewed-by: Akhil Velagapudi <avelagap@google.com>
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Jonathan Hart <jonohart@google.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -1164,7 +1164,7 @@ skip_vtep:
 			return ret;
 	}
 #endif /* TUNNEL_MODE && !IS_MULTI_NIC_DEVICE */
-	if (is_defined(ENABLE_HOST_ROUTING)) {
+	if (is_defined(ENABLE_HOST_ROUTING) && !is_defined(IS_MULTI_NIC_DEVICE)) {
 		int oif;
 
 		ret = redirect_direct_v4(ctx, ETH_HLEN, ip4, ctx_get_ifindex(ctx), &oif);
