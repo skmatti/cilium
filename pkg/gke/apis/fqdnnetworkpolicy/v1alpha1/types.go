90a3d802605e74693b62e015ab9a6c93605e1f61
Rahul Joshi <rkjoshi@google.com>
2022-08-07 17:51:04 -0400

    FQDN NetworkPolicy pattern supports "*" wildcard
    Updating the kube-builder validation pragma to allow specifying `Pattern:
    "*"` as a valid input. Also relaxed the validation pragma slightly which
    makes it easier to read and presents a less intimidating output to users
    if they hit a validation error.
    
    Examples of the regex are checked here:
      - For Name: https://regex101.com/r/TapD1k/1
      - For Pattern: https://regex101.com/r/zX0nnT/1
    
    Bug: 229105221
    Change-Id: I0c8f51460c17d17415868a7fb2c73caf7f382080

--- pkg/gke/apis/fqdnnetworkpolicy/v1alpha1/types.go
+++ pkg/gke/apis/fqdnnetworkpolicy/v1alpha1/types.go
@@ -56,9 +56,9 @@ type FQDNNetworkPolicySpec struct {
 	Egress []FQDNNetworkPolicyEgressRule `json:"egress"`
 }
 
-// FQDNPolicyEgressRule identifies a set of endpoints traffic is allowed
-// to. The exact L4 endpoints are constructed as a cross-product between
-// the matches and ports lists.
+// FQDNNetworkPolicyEgressRule identifies a set of endpoints traffic is allowed
+// to. The exact L4 endpoints are constructed as a cross-product between the
+// matches and ports lists.
 type FQDNNetworkPolicyEgressRule struct {
 	// Matches specifies the FQDN peers to which egress traffic is allowed.
 	// Matches may not be missing or empty - it must contain at least 1
@@ -75,8 +75,10 @@ type FQDNNetworkPolicyEgressRule struct {
 	Ports []FQDNNetworkPolicyPort `json:"ports,omitempty"`
 }
 
-// Exactly one of the sub-fields of FQDNNetworkPolicyMatch must be set. An empty
-// struct is not allowed. A struct setting both Name and Pattern is not allowed.
+// FQDNNetworkPolicyMatch specifies which FQDNs are allowed as peers of the
+// selected pods.Exactly one of the sub-fields of FQDNNetworkPolicyMatch must be
+// set. An empty struct is not allowed. A struct setting both Name and Pattern
+// is not allowed.
 //
 // +kubebuilder:validation:MinProperties=1
 // +kubebuilder:validation:MaxProperties=1
@@ -85,7 +87,7 @@ type FQDNNetworkPolicyMatch struct {
 	// no other match types may be specified in the same struct.
 	//
 	// +optional
-	// +kubebuilder:validation:Pattern=`^([a-zA-Z0-9]([-a-zA-Z0-9_]*[a-zA-Z0-9])*\.)+([a-zA-Z0-9]([-a-zA-Z0-9_]*[a-zA-Z0-9])*)$`
+	// +kubebuilder:validation:Pattern=`^([a-zA-Z0-9]([-a-zA-Z0-9_]*[a-zA-Z0-9])*\.?)*$`
 	Name string `json:"name,omitempty"`
 
 	// Pattern allows matching FQDNs using wildcard specifiers. If this
@@ -93,29 +95,37 @@ type FQDNNetworkPolicyMatch struct {
 	// struct.
 	// "*" matches 0 or more DNS valid characters (except for "."), and may occur
 	// anywhere in the pattern.
+	// As a special case, the "*" character by itself acts as a wildcard,
+	// matching all domain names.
 	//
 	// Examples:
-	// `*.google.com` matches subdomains of google.com at that level
-	//     "www.google.com" and "mail.google.com" match, however "google.com",
-	//     "sub.subdomain.google.com", and "kubernetes.io" do not.
-	// `sub*.google.com` matches subdomains of google.com where the subdomain
-	// component begins with "sub"
-	//     "sub.google.com" and "subdomain.google.com" match, however
-	//     "www.google.com", "mail.google.com", and "google.com do not.
-	// `*.*.google.com` matches subdomains of google.com at that level
-	//     "sub.subdomain.google.com" matches, however "google.com" and
-	//     "mail.google.com" do not.
-	// `*a*.google.com` matches subdomains of google.com that contain an "a" at
-	// that level
-	//     "mail.google.com", "maps.google.com", and
-	//     "subdomain.maps.google.com" match, however "google.com" and
-	//     "cloud.google.com" do not.
+	//
+	//   - `*.google.com` matches subdomains of google.com at that level
+	//     - "www.google.com" and "mail.google.com" match, however "google.com",
+	//       "sub.subdomain.google.com", and "kubernetes.io" do not.
+	//   - `sub*.google.com` matches subdomains of google.com where the subdomain
+	//     component begins with "sub"
+	//     - "sub.google.com" and "subdomain.google.com" match, however
+	//       "www.google.com", "mail.google.com", and "google.com do not.
+	//   - `*.*.google.com` matches subdomains of google.com at that level
+	//     - "sub.subdomain.google.com" matches, however "google.com" and
+	//       "mail.google.com" do not.
+	//   - `*a*.google.com` matches subdomains of google.com that contain an "a" at
+	//     that level
+	//     - "mail.google.com", "maps.google.com", and
+	//       "subdomain.maps.google.com" match, however "google.com" and
+	//       "cloud.google.com" do not.
+	//   - `*` by itself is a wild-card that matches all domains
+	//     - "www.google.com", "kubernetes.io", and "sub.subdomain.google.com"
+	//       all match
 	//
 	// +optional
-	// +kubebuilder:validation:Pattern=`^([a-zA-Z0-9*]([-a-zA-Z0-9_*]*[a-zA-Z0-9*])*\.)+([a-zA-Z0-9*]([-a-zA-Z0-9_*]*[a-zA-Z0-9*])*)$`
+	// +kubebuilder:validation:Pattern=`^([a-zA-Z0-9*]([-a-zA-Z0-9_*]*[a-zA-Z0-9*])*\.?)*$`
 	Pattern string `json:"pattern,omitempty"`
 }
 
+// FQDNNetworkPolicyPort specifies which remote port and protocol is a valid
+// peer of the selected pod.
 type FQDNNetworkPolicyPort struct {
 	// Protocol is the L4 protocol. Valid options are "TCP", "UDP",
 	// or "ALL".
