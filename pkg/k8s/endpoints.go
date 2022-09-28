f2913830d17d526049dc1399d2f2b073300649ac
Harsh Modi <harshmodi@google.com>
2022-09-28 15:11:54 -0700

    [backport] sctp: Handle SCTP when correlating Endpoints to services.
    Otherwise, Services with SCTP ports and pods with SCTP ports will not
    have their backends correctly populated.
    
    This was missed in the original SCTP PR
    (b1d9fcd20042ad95a3391ade10234ad8eed62a1c).
    
    This is backported from upstream master to the v1.12 branch
    
    Change-Id: I1c1f02e2eb6159984363a813aa4b48343a9b4c4c
    Signed-off-by: Harsh Modi <harshmodi@google.com>

--- pkg/k8s/endpoints.go
+++ pkg/k8s/endpoints.go
@@ -224,6 +224,8 @@ func parseEndpointPortV1Beta1(port slim_discovery_v1beta1.EndpointPort) (string,
 			proto = loadbalancer.TCP
 		case slim_corev1.ProtocolUDP:
 			proto = loadbalancer.UDP
+		case slim_corev1.ProtocolSCTP:
+			proto = loadbalancer.SCTP
 		default:
 			return "", nil
 		}
@@ -317,6 +319,8 @@ func parseEndpointPortV1(port slim_discovery_v1.EndpointPort) (string, *loadbala
 			proto = loadbalancer.TCP
 		case slim_corev1.ProtocolUDP:
 			proto = loadbalancer.UDP
+		case slim_corev1.ProtocolSCTP:
+			proto = loadbalancer.SCTP
 		default:
 			return "", nil
 		}
