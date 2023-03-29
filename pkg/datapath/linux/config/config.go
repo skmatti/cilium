a015e855719f848f43fa9460e03d6793be2b888f
Damian Sawicki <dsawicki@google.com>
2023-03-29 15:25:52 +0000

    #define for Transparent Health Checks
    #1n #thc
    
    Change-Id: Ib0cdc714c01037a971acf727acbf60d047ca3035
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/658218
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Katarzyna Łach <katarzynalach@google.com>
    Reviewed-by: Varun Marupadi <varunmar@google.com>

--- pkg/datapath/linux/config/config.go
+++ pkg/datapath/linux/config/config.go
@@ -633,6 +633,10 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 	cDefinesMap["CIDR_IDENTITY_RANGE_START"] = fmt.Sprintf("%d", identity.MinLocalIdentity)
 	cDefinesMap["CIDR_IDENTITY_RANGE_END"] = fmt.Sprintf("%d", identity.MaxLocalIdentity)
 
+	if option.Config.EnableTHC {
+		cDefinesMap["TRANSPARENT_HEALTH_CHECK"] = "1"
+	}
+
 	// Since golang maps are unordered, we sort the keys in the map
 	// to get a consistent written format to the writer. This maintains
 	// the consistency when we try to calculate hash for a datapath after
