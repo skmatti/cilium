6bec7452f210789d0fce1377588442a37cbe25c6
Alan Kutniewski <kutniewski@google.com>
2023-08-03 13:56:25 +0200

    operator: Implement Full Reconciliation of CES
    [Backport commit f651ab594321f7b2333fefcb8602ec4f5b5a062f from PR
    https://github.com/cilium/cilium/pull/26836]
    
    Refactor CES Controller:
    - Limit the functionality of the Manager to maintain mapping between CEPs and CESs.
    - Implement reconciliation in the Reconcilar based on the mapping from the Manager and CEPs and CESs from the watchers stores.
    - React to the CESs events in addition to the CEPs events.
    
    Change-Id: I28112d5a5d6949116081d71d9e054d327d75cafc
    Signed-off-by: Alan Kutniewski <kutniewski@google.com>
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/820741
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Akhil Velagapudi <avelagap@google.com>
    Reviewed-by: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Dorde Lapcevic <dordel@google.com>

--- operator/pkg/ciliumendpointslice/endpointslice_test.go
+++ operator/pkg/ciliumendpointslice/endpointslice_test.go
@@ -4,28 +4,126 @@
 package ciliumendpointslice
 
 import (
+	"context"
+	"sync"
 	"testing"
+	"time"
 
-	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/google/go-cmp/cmp"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/stretchr/testify/assert"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
+
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	capi_v2a1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
 )
 
-func createCESWithIDs(cesName string, ids []int64) *v2alpha1.CiliumEndpointSlice {
-	ces := &v2alpha1.CiliumEndpointSlice{ObjectMeta: metav1.ObjectMeta{Name: cesName}}
+func createManagerEndpoint(name string, identity int64) capi_v2a1.CoreCiliumEndpoint {
+	return capi_v2a1.CoreCiliumEndpoint{
+		Name:       name,
+		IdentityID: identity,
+	}
+}
+
+func createStoreEndpoint(name string, namespace string, identity int64) *v2.CiliumEndpoint {
+	return &v2.CiliumEndpoint{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Status: v2.EndpointStatus{
+			Identity: &v2.EndpointIdentity{
+				ID: identity,
+			},
+			Networking: &v2.EndpointNetworking{},
+		},
+	}
+}
+
+func createStoreEndpointSlice(name string, namespace string, endpoints []capi_v2a1.CoreCiliumEndpoint) *capi_v2a1.CiliumEndpointSlice {
+	return &capi_v2a1.CiliumEndpointSlice{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: name,
+		},
+		Namespace: namespace,
+		Endpoints: endpoints,
+	}
+}
+
+func createCESWithIDs(cesName string, ids []int64) *capi_v2a1.CiliumEndpointSlice {
+	ces := &capi_v2a1.CiliumEndpointSlice{ObjectMeta: meta_v1.ObjectMeta{Name: cesName}}
 	for _, id := range ids {
-		cep := v2alpha1.CoreCiliumEndpoint{IdentityID: id}
+		cep := capi_v2a1.CoreCiliumEndpoint{IdentityID: id}
 		ces.Endpoints = append(ces.Endpoints, cep)
 	}
 	return ces
 }
 
-func assertEqualIDs(t *testing.T, wantIdentities, gotIdentities map[string]bool) {
-	t.Helper()
-	if diff := cmp.Diff(wantIdentities, gotIdentities); diff != "" {
-		t.Errorf("Unexpected Identites in the CES store (-want +got): \n%s", diff)
-	}
+func TestSyncCESsInLocalCache(t *testing.T) {
+	_, c := client.NewFakeClientset()
+	cesController := NewCESController(context.Background(), &sync.WaitGroup{}, c, 5, "", 10, 20)
+
+	cep1 := createManagerEndpoint("cep1", 1)
+	cep2 := createManagerEndpoint("cep2", 1)
+	cep3 := createManagerEndpoint("cep3", 2)
+	cep4 := createManagerEndpoint("cep4", 2)
+	ces1 := createStoreEndpointSlice("ces1", "ns", []capi_v2a1.CoreCiliumEndpoint{cep1, cep2, cep3, cep4})
+	ceSliceStore.Add(ces1)
+	cep5 := createManagerEndpoint("cep5", 1)
+	cep6 := createManagerEndpoint("cep6", 1)
+	cep7 := createManagerEndpoint("cep7", 2)
+	ces2 := createStoreEndpointSlice("ces2", "ns", []capi_v2a1.CoreCiliumEndpoint{cep5, cep6, cep7})
+	ceSliceStore.Add(ces2)
+
+	cesController.syncCESsInLocalCache()
+
+	mapping := cesController.Manager.(*cesManagerFcfs).mapping
+
+	cesN, _ := mapping.getCESName("ns/cep1")
+	assert.Equal(t, cesN, CESName("ces1"))
+	cesN, _ = mapping.getCESName("ns/cep2")
+	assert.Equal(t, cesN, CESName("ces1"))
+	cesN, _ = mapping.getCESName("ns/cep3")
+	assert.Equal(t, cesN, CESName("ces1"))
+	cesN, _ = mapping.getCESName("ns/cep4")
+	assert.Equal(t, cesN, CESName("ces1"))
+	cesN, _ = mapping.getCESName("ns/cep5")
+	assert.Equal(t, cesN, CESName("ces2"))
+	cesN, _ = mapping.getCESName("ns/cep6")
+	assert.Equal(t, cesN, CESName("ces2"))
+	cesN, _ = mapping.getCESName("ns/cep7")
+	assert.Equal(t, cesN, CESName("ces2"))
+}
+
+func TestEnqueueingPreIntitialization(t *testing.T) {
+	_, c := client.NewFakeClientset()
+
+	cesController := NewCESController(context.Background(), &sync.WaitGroup{}, c, 5, "", 10, 20)
+	assert.Equal(t, 0, cesController.queue.Len())
+
+	cep1 := createStoreEndpoint("cep1", "ns", 1)
+	cep2 := createStoreEndpoint("cep2", "ns", 1)
+	cep3 := createStoreEndpoint("cep3", "ns", 2)
+	cesController.OnEndpointUpdate(cep1)
+	cesController.OnEndpointUpdate(cep2)
+	cesController.OnEndpointUpdate(cep3)
+	cesController.OnEndpointDelete(cep2)
+	ces1 := createStoreEndpointSlice("ces1", "ns", []capi_v2a1.CoreCiliumEndpoint{createManagerEndpoint("cep1", 1), createManagerEndpoint("cep3", 2)})
+	ceSliceStore.Add(ces1)
+	ces2 := createStoreEndpointSlice("ces2", "ns", []capi_v2a1.CoreCiliumEndpoint{createManagerEndpoint("cep2", 1)})
+	ceSliceStore.Add(ces2)
+	time.Sleep(DefaultCESSyncTime * 2) // elements are added to the queue with a default delay
+	assert.Equal(t, 0, cesController.queue.Len())
+	assert.Equal(t, 4, len(cesController.preInitEnqueuedEndpointsEvents))
+	assert.Equal(t, false, cesController.endpointsMappingInitialized)
+
+	cesController.syncCESsInLocalCache()
+	assert.Equal(t, true, cesController.endpointsMappingInitialized)
+
+	cesController.processEnqueuedPreInitEndpoints()
+	time.Sleep(DefaultCESSyncTime * 2) // elements are added to the queue with a default delay
+	assert.Equal(t, 2, cesController.queue.Len())
 }
 
 func TestUsedIdentitiesInCESs(t *testing.T) {
@@ -57,4 +155,11 @@ func TestUsedIdentitiesInCESs(t *testing.T) {
 	wantIdentities["50"] = true
 	gotIdentities = usedIdentitiesInCESs(cesStore)
 	assertEqualIDs(t, wantIdentities, gotIdentities)
+}
+
+func assertEqualIDs(t *testing.T, wantIdentities, gotIdentities map[string]bool) {
+	t.Helper()
+	if diff := cmp.Diff(wantIdentities, gotIdentities); diff != "" {
+		t.Errorf("Unexpected Identites in the CES store (-want +got): \n%s", diff)
+	}
 }
